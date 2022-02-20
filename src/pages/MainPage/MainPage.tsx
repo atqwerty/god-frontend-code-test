e16372c620ad63cb7b4d42444e8507ed1892fea4
Denis Markitanov <d.markitanov@vlife.kz>
2022-02-20 22:44:12 +0100

    feat(feat/main-page-layout): simple main page layout

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { View } from "vcc-ui";
 
+// justifyContent prop of View doesn't work
+
 const MainPage: React.FC = () => {
-  return <View height={"100%"}>adsf</View>;
+  return (
+    <View
+      height={"100%"}
+      extend={{
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ background: "green", width: "500px", height: "300px" }}>
+        adsf
+      </div>
+    </View>
+  );
 };
 
 export default MainPage;
