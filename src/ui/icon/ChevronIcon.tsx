a930a5051a3f3df4ef3700cc8ae6bb87d91048f3
Denis Markitanov <d.markitanov@vlife.kz>
2022-02-21 22:51:13 +0100

    fix(feat/car-items): removed uneccessary files from ui folder

--- src/ui/icon/ChevronIcon.tsx
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export default styled.svg.attrs({
-  version: 1.0,
-  xmlns: "../../../docs/chevron-circled.svg",
-})``;
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,3 +1,1 @@
-export * from "./chevron";
-export * from "./icon";
 export * from "./chevronCircled";
