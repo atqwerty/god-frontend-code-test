c2c2ecade39811052f3b675110a79769369e5f6d
Denis Markitanov <d.markitanov@vlife.kz>
2022-02-22 16:52:57 +0100

    feat(feat/loader): added spinner to the carousel

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,22 @@
-import React from "react";
-import { View } from "vcc-ui";
+import React, { useState, useEffect } from "react";
+import { View, Spinner } from "vcc-ui";
 import { Carousel } from "./components";
+import { getCars } from "../../api/carRequest";
 
 // justifyContent prop of View doesn't work
 
 const MainPage: React.FC = () => {
+  const [cars, setCars] = useState([]);
+  useEffect(() => {
+    if (cars.length === 0) {
+      getCars()
+        .then((res) => {
+          setTimeout(() => setCars(res), 500);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [cars]);
+
   return (
     <View
       extend={{
@@ -13,7 +25,15 @@ const MainPage: React.FC = () => {
       }}
     >
       <View padding={"0 50px 0 50px"}>
-        <Carousel />
+        {cars.length > 0 ? (
+          <Carousel cars={cars} />
+        ) : (
+          <React.Fragment>
+            <View extend={{ alignSelf: "center", justifySelf: "center" }}>
+              <Spinner size={48} />
+            </View>
+          </React.Fragment>
+        )}
       </View>
     </View>
   );
--- src/pages/MainPage/components/Carousel/Carousel.tsx
+++ src/pages/MainPage/components/Carousel/Carousel.tsx
@@ -1,43 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ItemsCarousel from "react-items-carousel";
 import { ChevronCircled } from "../../../../../src/ui";
-import { getCars } from "../../../../api/carRequest";
 import { CarItem } from "./components";
+import { View } from "vcc-ui";
 
-const Carousel: React.FC = () => {
+const Carousel: React.FC = ({ cars }) => {
   const [activeItemIndex, setActiveItemIndex] = useState(0);
-  const [cars, setCars] = useState([]);
   const chevronWidth = 50;
 
-  useEffect(() => {
-    getCars()
-      .then((res) => setCars(res))
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
-    <ItemsCarousel
-      requestToChangeActive={setActiveItemIndex}
-      activeItemIndex={activeItemIndex}
-      gutter={20}
-      numberOfCards={4}
-      leftChevron={<ChevronCircled rotationDegree="180deg" />}
-      rightChevron={<ChevronCircled />}
-      outsideChevron
-      infiniteLoop
-      chevronWidth={chevronWidth}
-      classes={{
-        wrapper: "carousel_wrapper",
-        itemsWrapper: "items_wrapper",
-        rightChevronWrapper: "right_chevron",
-        leftChevronWrapper: "left_chevron",
-      }}
-    >
-      {cars.length > 0 &&
-        cars.map((car) => {
+    <View>
+      <ItemsCarousel
+        requestToChangeActive={setActiveItemIndex}
+        activeItemIndex={activeItemIndex}
+        gutter={20}
+        numberOfCards={4}
+        leftChevron={<ChevronCircled rotationDegree="180deg" />}
+        rightChevron={<ChevronCircled />}
+        outsideChevron
+        infiniteLoop
+        chevronWidth={chevronWidth}
+        classes={{
+          wrapper: "carousel_wrapper",
+          itemsWrapper: "items_wrapper",
+          rightChevronWrapper: "right_chevron",
+          leftChevronWrapper: "left_chevron",
+        }}
+      >
+        {cars.map((car) => {
           return <CarItem carInfo={car} />;
         })}
-    </ItemsCarousel>
+      </ItemsCarousel>
+    </View>
   );
 };
 
